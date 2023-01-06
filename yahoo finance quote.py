064468f3c37feb6892ae8b73e41a5c733e37843b
RENCHI <raylrc@gmail.com>
2023-01-06 15:55:53 -0600

    Create yahoo finance quote.py

---
+++ yahoo finance quote.py
@@ -0,0 +1,9 @@
+keyword = "AAPL"
+URL = "https://finance.yahoo.com/quote/"
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+try:
+    r = requests.get(URL+keyword, headers = headers)
+    print(r.text)
+except:
+    print(r.text)
