e159ff97f2082e7ad2d2c1c86019796d5b83015b
RENCHI <raylrc@gmail.com>
2023-01-06 16:23:21 -0600

    Create getImage.py

---
+++ getImage.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+url = "https://renchiliu.com/upload/IMG_5822.JPG"
+root = "/home/pics/"   # be careful about the las / 
+path = root + url.split('/')[-1]
+
+
+try:
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(path):
+        r = requests.get(url)
+        print(r.status_code)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+            print("saved pic")
+    else:
+        print("file exists")
+except:
+    print("error")
